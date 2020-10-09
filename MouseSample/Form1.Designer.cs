7aec4a757d9f08a25e50351cd0c72c5f9bc31a14
ThansiJamal <thansijamal18@gmail.com>
2020-10-09 08:51:28 -0700

    mouse event

---
+++ MouseSample/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace MouseSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMouse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMouse
+            // 
+            this.btnMouse.Location = new System.Drawing.Point(167, 63);
+            this.btnMouse.Name = "btnMouse";
+            this.btnMouse.Size = new System.Drawing.Size(75, 40);
+            this.btnMouse.TabIndex = 0;
+            this.btnMouse.Text = "Mouse";
+            this.btnMouse.UseVisualStyleBackColor = true;
+            this.btnMouse.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnMouse_MouseDown);
+            this.btnMouse.MouseEnter += new System.EventHandler(this.btnMouse_MouseEnter);
+            this.btnMouse.MouseLeave += new System.EventHandler(this.btnMouse_MouseLeave);
+            this.btnMouse.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnMouse_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 217);
+            this.Controls.Add(this.btnMouse);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMouse;
+    }
+}
+
---
+++ MouseSample/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMouse_MouseEnter(object sender, EventArgs e)
+        {
+            btnMouse.BackColor = Color.Red;
+        }
+
+        private void btnMouse_MouseLeave(object sender, EventArgs e)
+        {
+            btnMouse.BackColor = Color.Yellow;
+        }
+
+        private void btnMouse_MouseUp(object sender, MouseEventArgs e)
+        {
+            btnMouse.BackColor = Color.Blue;
+        }
+
+        private void btnMouse_MouseDown(object sender, MouseEventArgs e)
+        {
+            btnMouse.BackColor = Color.Pink;
+        }
+    }
+}
